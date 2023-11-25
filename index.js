a6ebd9af21c0b3582296886e9541866aa5948e10
HalimaTusSadiya <halimasaadiya66@gmail.com>
2023-11-26 01:35:25 +0600

    created API to delete article

--- index.js
+++ index.js
@@ -145,6 +145,13 @@ async function run() {
     res.send(result)
   })
 
+  app.delete("/delete-article/:id", async(req,res)=> {
+    const id = req.params.id
+    const query = {_id: new ObjectId(id)}
+    const result = await articlesCollection.deleteOne(query)
+    res.send(result)
+  })
+
   // user related API
   app.post("/create-user", async(req, res)=> {
     const user = req.body;
